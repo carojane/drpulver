5f6c725c71df7947c93142f60a85ac8be24d7df7
carojane <carojane@gmail.com>
2014-10-06 19:47:52 -0400

    spec fix

--- spec/helpers/session_helper.rb
+++ spec/helpers/session_helper.rb
@@ -5,11 +5,6 @@ module SessionHelpers
     fill_in "Email", with: email
     fill_in "Password", with: password
     fill_in "Password confirmation", with: password
-    # select "Date of birth" "1999", "January", "1"
-    fill_in "About", with: "Lorem Ipsum"
-    fill_in "First name", with: "Jane"
-    fill_in "Last name", with: "Smith"
-    # split to first/last
     click_button "Sign up"
   end
 
