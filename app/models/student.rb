691179d4c7d9a9c06d2d605f47c0998a16b199f5
carojane <carojane@gmail.com>
2014-10-07 10:54:41 -0400

    student bio

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,17 @@
 class Student < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true, uniqueness: true
+
+  def full_name
+    first = self.first_name
+    last = self.last_name
+    if first && last
+      first + " " + last
+    elsif first
+      first
+    elsif last
+      last
+    else
+    end
+  end
 end
