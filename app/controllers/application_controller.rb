1d05f4dba29eb100cb179214bed924ad82d6deed
carojane <carojane@gmail.com>
2014-10-07 07:10:03 -0400

    hound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
                                                                        :user_id]]
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     student_path(current_user)
   end
 end
