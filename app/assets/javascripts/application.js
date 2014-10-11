d30fd20021be11c95028efdda23839fb0cda5aaf
carojane <carojane@gmail.com>
2014-10-10 23:40:55 -0400

    query text editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require jquery-te-1.4.0
+//= require jqueryte.js
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
