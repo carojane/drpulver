14d4f4c00a08dc69c074333ffa48c33b09428586
carojane <carojane@gmail.com>
2014-10-16 15:44:45 -0400

    views submissions

---
+++ spec/features/user_views_submissions_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "User views Submission", %{
+  As a professor, I want to see all submissions
+  So that I can grade them
+} do
+  let!(:submission) {FactoryGirl.create(:submission)}
+
+  scenario "on submissions index" do
+    admin = FactoryGirl.create(:user, role: "admin")
+    sign_in_as admin
+    visit course_assignment_submissions_path(submission.assignment.course,
+                                             submission.assignment)
+    save_and_open_page
+    expect(page).to have_content(submission.body)
+  end
+end
